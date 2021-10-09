d27dc3eff18fdfbfbd91a25f932488e53b86ac89
YanpengZhao1 <1569898677@qq.com>
2021-10-09 01:38:11 -0500

    final version

--- app/src/main/java/com/example/lab4_milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab4_milestone2/MainActivity.java
@@ -8,11 +8,19 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
@@ -40,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-        if (Build.VERSION.SDK_INT<23){
+        if(Build.VERSION.SDK_INT<23){
             startListening();
         }else{
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
@@ -52,6 +60,61 @@ public class MainActivity extends AppCompatActivity {
                     updateLocationInfo(location);
                 }
             }
+        }
+
+
+    }
+
+    private void startListening() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,@NonNull String[]permissions,@NonNull int[]grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            startListening();
+        }
+
+    }
+    public void updateLocationInfo(Location location){
+        Log.i("LocationInfo",location.toString());
+        TextView latTextView=(TextView) findViewById(R.id.Text2);
+        TextView lonTextView=(TextView) findViewById(R.id.Text3);
+        TextView altTextView=(TextView) findViewById(R.id.Text5);
+        TextView accTextView=(TextView) findViewById(R.id.Text4);
+        latTextView.setText("Latitude: "+location.getLatitude());
+        lonTextView.setText("Longitude: "+location.getLongitude());
+        altTextView.setText("Altitude: "+location.getAltitude());
+        accTextView.setText("Accuracy: "+location.getAccuracy());
+        Geocoder geocoder=new Geocoder(getApplicationContext(), Locale.getDefault());
+        try{
+            String address="Could not find address";
+            List<Address> listAddresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            if (listAddresses!=null && listAddresses.size()>0){
+                Log.i("PlaceInfo",listAddresses.get(0).toString());
+                address="Address: \n";
+                if(listAddresses.get(0).getSubThoroughfare()!=null){
+                    address+=listAddresses.get(0).getSubThoroughfare()+" ";
+                }
+                if(listAddresses.get(0).getThoroughfare()!=null){
+                    address+=listAddresses.get(0).getThoroughfare()+"\n";
+                }
+                if(listAddresses.get(0).getLocality()!=null){
+                    address+=listAddresses.get(0).getLocality()+"\n";
+                }
+                if(listAddresses.get(0).getPostalCode()!=null){
+                    address+=listAddresses.get(0).getPostalCode()+"\n";
+                }
+                if(listAddresses.get(0).getCountryName()!=null){
+                    address+=listAddresses.get(0).getCountryName()+"\n";
+                }
+            }
+            TextView addressTextView=(TextView) findViewById(R.id.Text6);
+            addressTextView.setText(address);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
